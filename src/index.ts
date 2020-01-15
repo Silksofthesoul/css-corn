d75774c8dd023e4cb591c44da0ef560e8b4034ba
Evgenij Labuzov <silksofthesoul@yandex.ru>
2020-01-15 06:41:55 +0300

    src changes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-const removeProperty = (prop: string): object => ({ [prop]: _, ...object }) => object;
+const removeProperty = (prop: string): object => ({ [prop]: _, ...object }): object => object;
 
-class Css {
+interface CssParams {
+  id: string
+}
+
+export class Css {
   styles: object;
   id: string;
   domStyle: HTMLElement | null;
 
-  constructor() {
+  constructor(_params?: CssParams) {
+    const params: CssParams = {
+      id: 'superstyle',
+      ..._params
+    };
     this.styles = {};
-    this.id = 'superstyle';
+    this.id = params.id;
     let old = [...document.querySelectorAll('#' + this.id)];
     old.forEach(item => {
       let parent: (Node & ParentNode) | null = item.parentNode;
@@ -15,11 +23,7 @@ class Css {
     });
     this.domStyle = document.createElement('style');
     this.domStyle.id = this.id;
-
-    this.domStyle.setAttribute('style', 'display:block; background: red; position: fixed;font-size:1.8em;');
-    document.head.setAttribute('style', 'display: block; background: red; position: fixed; top: 0; left: 0; z-index: 1111; padding: 2em;');
     this.domStyle.setAttribute('type', 'text/css');
-    this.domStyle.setAttribute('contenteditable', 'true');
     document.head.appendChild(this.domStyle);
   }
 
@@ -58,8 +62,9 @@ class Css {
       this.styles = removeProperty(selector)(this.styles);
     } else {
       let stArr = params.split(/[\s;,\/]/igm).filter(item => item.trim());
-      stArr
-        .forEach(item => this.styles[selector] = removeProperty(item)(this.styles[selector]));
+      stArr.forEach(item => {
+        this.styles[selector] = removeProperty(item)(this.styles[selector])
+      });
     }
     return this;
   }
@@ -70,39 +75,15 @@ class Css {
 
   render(): Css {
     const en = (o: object) => Object.entries(o);
-    const res = en(this.styles)
+    const res: string = en(this.styles)
       .map(
         item => `${item[0]}{${en(item[1])
           .map(itm => itm.join(':'))
           .join(';')};}`
       ).join('');
-    this.domStyle?.innerText = res;
-    console.log('iseethelight', JSON.stringify(this.styles));
+    this.domStyle.innerText = res;
     return this;
   }
 };
-
-function* gs(i: number = 0): Generator {
-  yield i++;
-  yield* gs(i);
-}
-const g = gs();
-const css: Css = new Css();
-
-const arr = [
-  () => { css.add('body', 'background: green; font-size: 2em;').render(); },
-  () => { css.add('body', 'background: black; color: #fff; font-size: 6em;').render(); },
-  () => { css.add('.message__title', 'transition:transform 0.25s 0s ease-out;display:block; transform:rotate(45deg);').render(); },
-  () => { css.add('body', 'background: black; color: #fff; font-size: 7em;').render(); },
-  () => { css.del('body').render(); },
-  () => { css.del('.message__title', 'transform, transition').render(); },
-  // ()=>{css.rename('.message__title', '.test > .text').render();},
-];
-function tp() {
-  let { value } = g.next();
-  if (value > arr.length - 1) return 0;
-  console.log(value);
-  arr[value]();
-  setTimeout(tp, 100);
-}
-tp();
+window.$Css = Css;
+export default Css;
