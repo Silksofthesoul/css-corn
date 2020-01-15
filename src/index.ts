568a78e4c26c238121e70fc9be0dc3ab16ef5241
silksofthesoul <silksofthesoul@yandex.ru>
2020-01-15 16:38:37 +0300

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,71 @@
-const removeProperty = (prop: string): object => ({ [prop]: _, ...object }): object => object;
+declare global {
+    interface Window { $Css: any; }
+}
+window.$Css = window.$Css || {};
 
-interface CssParams {
-  id: string
+
+// const removeProperty = (prop: string): object => ({ [prop]: undefined, ...object }: object): object => object;
+
+
+
+type TId = string;
+type TEqual = (a: any, b:any) => boolean;
+
+interface IProperty {
+  [property: string]: string
+};
+
+interface IStyle {
+  [selector: string]: IProperty
+};
+
+// console.log(isType([1,2,3,4], 'object'));
+interface ICss {
+  id: TId,
+  _id: TId,
+  styles: IStyle,
+  _styles: IStyle
 }
 
-export class Css {
-  styles: object;
-  id: string;
-  domStyle: HTMLElement | null;
+const isType: TEqual = (val: any, type: string): boolean => typeof val === type;
+isType('123', 'asd');
 
-  constructor(_params?: CssParams) {
-    const params: CssParams = {
-      id: 'superstyle',
+export class Css {
+  private _id: TId;
+  private _styles: IStyle;
+  private _element: HTMLElement | null;
+
+  constructor(_params?: ICss) {
+    const params: ICss = {
+      ...{} as ICss,
+      id: 'css-corn',
+      styles: <IStyle>{},
       ..._params
     };
-    this.styles = {};
-    this.id = params.id;
-    let old = [...document.querySelectorAll('#' + this.id)];
+    this._id = params.id;
+    this._styles = params._styles;
+    let old: Element[] = [...document.querySelectorAll(`#${this.id}`)];
     old.forEach(item => {
       let parent: (Node & ParentNode) | null = item.parentNode;
-      parent ?.removeChild(item);
+      parent?.removeChild(item);
     });
-    this.domStyle = document.createElement('style');
-    this.domStyle.id = this.id;
-    this.domStyle.setAttribute('type', 'text/css');
-    document.head.appendChild(this.domStyle);
+    this._element = document.createElement('style');
+    this._element.id = this.id;
+    this._element.setAttribute('type', 'text/css');
+    document.head.appendChild(this._element);
   }
 
-  parse(styles: string): object {
+  public set id(id: TId) {
+    if(this._element) this._element.id = id;
+    this._id = id;
+  }
+  public get id() { return this._id }
+
+  public set styles(val: IStyle) { this._styles = val; }
+  public get styles() { return this._styles }
+
+
+  private parse(styles: string): IStyle {
     return styles
       .split(';')
       .map(item => {
@@ -36,30 +74,32 @@ export class Css {
           .map(itm => itm ? itm.trim() : false)
           .filter(itm => itm);
       })
-      // // WARNING: WTF?
-      .reduce((stl: object, current: (string | false)[]) => {
-        if (current && current[1] && current[0]) stl[current[0]] = current[1];
+      .reduce((stl: IStyle, current: string[]) => {
+        console.log(stl, '11');
+        console.log(current, '22');
+        if (current && current.length === 2 && current[0]) stl[current[0]] = current[1];
         return stl;
-      }, {});
+      }, {} as IStyle);
   }
-
-  stringify(obj: object): string {
+  private stringify(obj: IStyle): string {
     return Object.entries(obj)
       .map(item => [item[0], item[1]].join(':'))
       .join(';') + ';';
   }
 
-  getStyle(params: string): object { return this.parse(params); }
+  private getStyle(styleString: string): IStyle { return this.parse(styleString); }
 
-  add(selector: string, params: string): Css {
-    let styles = this.getStyle(params);
-    this.styles[selector] = { ...this.styles[selector], ...styles };
+  public add(selector: string, styleString: string): this {
+    let styles = this.getStyle(styleString);
+    this.styles[selector] = <IStyle>{ ...this.styles[selector], ...styles };
     return this;
   }
 
+  /*
+
   del(selector: string, params: string = ''): Css {
     if (!params) {
-      this.styles = removeProperty(selector)(this.styles);
+      this.styles = <ICssStyle>removeProperty(selector)(this.styles: ICssStyle);
     } else {
       let stArr = params.split(/[\s;,\/]/igm).filter(item => item.trim());
       stArr.forEach(item => {
@@ -74,6 +114,10 @@ export class Css {
   }
 
   render(): Css {
+    if (!this.element) {
+      console.error(`DOM element <style> is not exist`);
+      return this;
+    }
     const en = (o: object) => Object.entries(o);
     const res: string = en(this.styles)
       .map(
@@ -81,9 +125,12 @@ export class Css {
           .map(itm => itm.join(':'))
           .join(';')};}`
       ).join('');
-    this.domStyle.innerText = res;
+
+    this.element.innerText = res;
     return this;
   }
+  */
 };
+
 window.$Css = Css;
 export default Css;
